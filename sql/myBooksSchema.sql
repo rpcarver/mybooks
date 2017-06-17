cc8643d1b06844264a3341c88b8e1829ce76038b
rpcarver <randy@blueottersoftware.com>
2017-06-16 22:26:21 -0600

    Database creation

---
+++ sql/myBooksSchema.sql
@@ -0,0 +1,151 @@
+CREATE DATABASE  IF NOT EXISTS `mybooks` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `mybooks`;
+-- MySQL dump 10.13  Distrib 5.7.17, for macos10.12 (x86_64)
+--
+-- Host: 127.0.0.1    Database: mybooks
+-- ------------------------------------------------------
+-- Server version	5.7.14-google-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED='5283838b-50a9-11e7-a24e-42010a800053:1-264354';
+
+--
+-- Table structure for table `authors`
+--
+
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors` (
+  `authorID` int(11) NOT NULL AUTO_INCREMENT,
+  `lastName` varchar(45) DEFAULT NULL,
+  `firstName` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`authorID`),
+  UNIQUE KEY `authorID_UNIQUE` (`authorID`),
+  KEY `lastName_idx` (`lastName`),
+  KEY `firstName_idx` (`firstName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `bookID` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(100) NOT NULL,
+  `publisherID` int(11) DEFAULT NULL,
+  `ISBN` int(13) DEFAULT NULL,
+  `printingYear` int(11) DEFAULT NULL,
+  `printingNum` int(11) DEFAULT NULL,
+  `formatID` int(11) DEFAULT NULL,
+  `rating` int(11) DEFAULT NULL,
+  `notes` varchar(500) DEFAULT NULL,
+  `locationName` varchar(45) NOT NULL,
+  PRIMARY KEY (`bookID`),
+  UNIQUE KEY `bookID_UNIQUE` (`bookID`),
+  KEY `title_idx` (`title`),
+  KEY `publisherID_idx` (`publisherID`),
+  KEY `ISBN_idx` (`ISBN`),
+  KEY `rating_idx` (`rating`),
+  KEY `locationName` (`locationName`),
+  KEY `formatID_fk_idx` (`formatID`),
+  CONSTRAINT `formatID_fk` FOREIGN KEY (`formatID`) REFERENCES `formats` (`formatID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `locationName_fk` FOREIGN KEY (`locationName`) REFERENCES `locations` (`locationName`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `publisherID_fk` FOREIGN KEY (`publisherID`) REFERENCES `publishers` (`publisherID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `books_authors`
+--
+
+DROP TABLE IF EXISTS `books_authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books_authors` (
+  `bookID` int(11) NOT NULL,
+  `authorID` int(11) NOT NULL,
+  KEY `bookID_idx` (`bookID`),
+  KEY `authorID_idx` (`authorID`),
+  CONSTRAINT `authorID` FOREIGN KEY (`authorID`) REFERENCES `authors` (`authorID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `bookID` FOREIGN KEY (`bookID`) REFERENCES `books` (`bookID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `formats`
+--
+
+DROP TABLE IF EXISTS `formats`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `formats` (
+  `formatID` int(11) NOT NULL AUTO_INCREMENT,
+  `formatName` varchar(45) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`formatID`),
+  UNIQUE KEY `formatID_UNIQUE` (`formatID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `locations`
+--
+
+DROP TABLE IF EXISTS `locations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `locations` (
+  `locationName` varchar(100) NOT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`locationName`),
+  UNIQUE KEY `locationName_UNIQUE` (`locationName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `publishers`
+--
+
+DROP TABLE IF EXISTS `publishers`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `publishers` (
+  `publisherID` int(11) NOT NULL AUTO_INCREMENT,
+  `publisherName` varchar(150) NOT NULL,
+  PRIMARY KEY (`publisherID`),
+  UNIQUE KEY `publisherID_UNIQUE` (`publisherID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-06-16 22:19:57
